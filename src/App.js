ba492a9086db43656872db26d786a933d846d75f
Muhammad Rafi Atha Zhafran <muhammad.rafi07@ui.ac.id>
2023-03-08 00:07:33 +0700

    set up tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo-plabs-full.png';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+
     </div>
   );
 }
