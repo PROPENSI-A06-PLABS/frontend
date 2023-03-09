f906cc0b27229ded96af38f171640ec57acabaa7
Raihan Adliputra <raihanadliputra@topsmac.local>
2023-03-09 17:30:04 +0700

    add dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo-plabs-full.png';
 import './App.css';
+import {DropDownListComponent} from '@syncfusion/ej2-react-dropdowns'
 import Button from './components/Button';
+
+// import Dropdown from './components/Dropdown';
+// import { useState } from 'react';
 
 // function App() {
 //   return (
@@ -11,8 +15,22 @@ import Button from './components/Button';
 //     </div>
 //   );
 // }
+// const options = [
+//   {key : 1, value : "hantop"},
+//   {key : 2, value : "pitjay"},
+//   {key : 3, value : "burungbpkgw"},
+//   {key : 4, value : "yongs"},
+//   {key : 5, value : "rorski"}
+// ];
 
 function App() {
+  const divStyle = {
+    margin: 50,
+    width: 130
+  }
+
+  // const [selectedOption, setSelectedOption] = useState("dion");
+  // const [open, setOpen] = useState(false);
   return (
     <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
       <p className="text-3xl text-gray-700 font-bold mb-5">
@@ -24,8 +42,22 @@ function App() {
 
       <button className="primary-button">asdad</button>
       <Button color="primary-button-success" name="blue" size="medium"></Button>
+      <div style={divStyle}>
+        <DropDownListComponent dataSource={["Hantop", "Pitjay", "Athuy", "Yong", "Rors"]}></DropDownListComponent>
+      </div>
+      {/* <Dropdown
+        options={options}
+        onChange={ (item) => setSelectedOption(item) }
+        selectedkey={selectedOption}
+        placeholder={"type to search"}
+        open={open}
+        setOpen={setOpen}
+      />
+      <p className="text-gray-500 text-lg">
+        selected option : {selectedOption}
+      </p> */}
     </div>
   );
-}
+} 
 
 export default App;
---
+++ src/components/Dropdown.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import '../App.css';
+
+const Dropdown = ({
+    color = "blue",
+    name = "name",
+    disabled = false,
+    onClick}) => {
+    const onButtonClick = () => { 
+        if(onClick){  onClick(name); 
+        }
+    } 
+    return( 
+    <div className="button-container">   
+        <button disabled={disabled} className={`${color} rounded-xl shadow border-dashed border-2 border-sky-500 p-8 m-10`} onClick={onButtonClick}>{name}
+        </button>  
+    </div> 
+    )
+}
+
+export default Dropdown;
