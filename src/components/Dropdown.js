d902809a78a233cac3b908387563e4cd1f6d4ac1
Raihan Adliputra <raihanadliputra@topsmac.local>
2023-03-11 21:03:58 +0700

    add check in button

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -12,7 +12,7 @@ const Dropdown = ({ options }) => {
         <div class="h-screen w-screen bg-gray-200 flex justify-center items-center">
             <div class="relative inline-block text-left">
                 <div>
-                    <button onClick={clickHandler} type="button" class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
+                    <button onClick={clickHandler} type="button" data-dropdown-delay="500" data-dropdown-trigger="hover" class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
                     Options
                     <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                         <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
@@ -20,11 +20,13 @@ const Dropdown = ({ options }) => {
                     </button>
                 </div>
                 {showOptions && (
-                    <div class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
-                        <div class="py-1" role="none">
-                            {options && options.map((option) => (
-                                <a href="/" class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem" tabindex="-1" id="menu-item-0">{option}</a>
-                            ))}
+                    <div class="py-2">
+                        <div class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                            <div class="py-1" role="none">
+                                {options && options.map((option) => (
+                                    <a href="/" class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem" tabindex="-1" id="menu-item-0">{option}</a>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 )}
